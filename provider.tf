1cf6eff093494c9a54f5ed30026e7bcc42eef4de
mailloku <122742549+mailloku@users.noreply.github.com>
2023-01-17 07:37:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAWNFBHWLEFZDY2TUH"
+  access_key = "AKIAWNFBHWLEFZDY2"
   secret_key = "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"
  
 }
