152b23226d2a4b5cf7cc7075c057c8fb007975da
mailloku <122742549+mailloku@users.noreply.github.com>
2023-01-17 05:37:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0ceecbb0f30a902a6"
   instance_type = "t2.micro"
- count=10 
-add commit to the file
+  count=2   
   tags = {
     Name = "HelloWorld"
   }
